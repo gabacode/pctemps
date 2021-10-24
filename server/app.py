39ae8fbcc06ad945d8793ebd41d306aa649e77d3
gabacode <gaba41@gmail.com>
2021-10-24 03:56:53 +0200

    update

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from config import *
 from datetime import datetime
-from flask import Flask
-import json, pythoncom, wmi
+from flask import Flask, jsonify
+from flask_cors import CORS
+import pythoncom, wmi
 
 app = Flask(__name__)
+CORS(app)
 data = []
 
 def check():
@@ -11,11 +13,12 @@ def check():
     w = wmi.WMI(namespace="root\OpenHardwareMonitor")
     temperature_infos = w.Sensor()
     data.clear()
-    data.append({"Time": datetime.now()})
+    data.append({'Name': "Time", 'Value': datetime.now()})
     for sensor in temperature_infos:
         if sensor.SensorType==u'Temperature':
-            data.append({"Name": sensor.Name, "Value": sensor.Value})
-    return json.dumps(data, default=str)
+            data.append({'Name': sensor.Name, 'Value': sensor.Value})
+    data.sort(key=lambda x: x['Name'])
+    return jsonify(data)
 
 @app.route("/")
 def home():
